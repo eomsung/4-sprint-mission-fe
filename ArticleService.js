98d5e371269f16632220f900a9606cdb9638af42
sungmin <usm0259@kaist.ac.kr>
2024-11-21 18:10:59 +0900

    make ArticleService.js

---
+++ ArticleService.js
@@ -0,0 +1,99 @@
+const BASE_URL = "https://sprint-mission-api.vercel.app/articles";
+
+const getArticle = async (id) => {
+  try {
+    if (!id) {
+      throw new Error("For 'id': Required field is not provided.");
+    }
+    const res = await fetch(`${BASE_URL}id=${id}`, {
+      method: "GET",
+    });
+    if (!res.ok) {
+      throw new Error(`Error: response status is ${res.status}`);
+    }
+    return res.json();
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const getArticleList = async (page = 1, pageSize = 100, keyword = "") => {
+  try {
+    const res = await fetch(
+      `${BASE_URL}?page=${page}&pageSize=${pageSize}&keyword=${keyword}`,
+      {
+        method: "GET",
+      }
+    );
+    if (!res.ok) {
+      throw new Error(`Error: response status is ${res.status}`);
+    }
+    return res.json();
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const createArticle = async (title, content, image) => {
+  const data = {
+    title,
+    content,
+    image,
+  };
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (!res.ok) {
+      throw new Error(`Error: response status is ${res.status}`);
+    }
+    return res.json();
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+const patchArticle = async (id, title, content, image) => {
+  try {
+    if (!id) {
+      throw new Error("For 'id': Required field is not provided.");
+    }
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        content,
+        image,
+      }),
+    });
+    if (!res.ok) {
+      throw new Error(`Error: response status is ${res.status}`);
+    }
+    return res.json();
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const deleteArticle = async (id) => {
+  try {
+    if (!id) {
+      throw new Error("For 'id': Required field is not provided.");
+    }
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`Error: response status is ${res.status}`);
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+};
