45adc1c1982f123275403c5d8af44f36e8776a9a
sungmin <usm0259@kaist.ac.kr>
2025-01-27 15:51:15 +0900

    wip

--- app/(provider)/(root)/products/page.jsx
+++ app/(provider)/(root)/products/page.jsx
@@ -7,7 +7,7 @@ function ProductPage() {
     <div>
       <Link href="/products/553">
         553번 물건 보러 가기/ 테스트용 /아이디 :testtest0000@test.com
-        /비밀번호:12345678{" "}
+        /비밀번호:12345678
       </Link>
     </div>
   );
