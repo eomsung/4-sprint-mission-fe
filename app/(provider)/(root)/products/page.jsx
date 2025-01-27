fc9f2f0a8e50e71519453c6c96192809c173306f
sungmin <usm0259@kaist.ac.kr>
2025-01-27 16:12:00 +0900

    test

--- app/(provider)/(root)/products/page.jsx
+++ app/(provider)/(root)/products/page.jsx
@@ -5,10 +5,8 @@ import React from "react";
 function ProductPage() {
   return (
     <div>
-      <Link href="/products/553">
-        553번 물건 보러 가기/ 테스트용 /아이디 :testtest0000@test.com
-        /비밀번호:12345678
-      </Link>
+      <Link href="/products/553">553번 물건 보러 가기/ 테스트용</Link>
+      <p> /아이디 :testtest0000@test.com /비밀번호:12345678</p>
     </div>
   );
 }
